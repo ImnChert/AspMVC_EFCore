e3d405235cfe36ce3639fb717d5c11a24498c57a
Osten Power <osten_power@mail.ru>
2023-04-12 13:24:44 +0300

    start huntingcontroller

--- HunterWeb/ViewModels/HuntingSeasonViewModel.cs
+++ HunterWeb/ViewModels/HuntingSeasonViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class HuntingSeasonViewModel
     {
+        public int Id { get; set; }
         public DateTime DateStart { get; set; }
         public DateTime DateEnd { get; set; }
         public InfoHuntingSeasonViewModel? InformationHuntingSeason { get; set; }
