04e45f2b663197bcf082a5a7b6c7047a27661142
Osten Power <osten_power@mail.ru>
2023-04-24 16:16:24 +0300

    rebuild BaseRepository

---
+++ DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Entities;
+using DAL.Intefaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories
+{
+    public abstract class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        protected DbContext _dbContext;
+
+        public BaseRepository(DbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public void Create(TEntity entity)
+            => _dbContext.Add(entity);
+
+        public void Update(TEntity entity)
+            => _dbContext.Update(entity);
+
+        public void Remove(TEntity entity)
+            => _dbContext.Remove(entity);
+
+        public async Task Save()
+            => await _dbContext.SaveChangesAsync();
+
+        public abstract Task<TEntity> GetById(int id);
+
+        public abstract IEnumerable<TEntity> GetAll();
+
+        public abstract IEnumerable<TEntity> GetByPredicate(Func<TEntity, bool> predicate);
+    }
+}
