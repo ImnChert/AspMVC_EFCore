eb112c17ed22484c4172c704fe022724a1c71709
Osten Power <osten_power@mail.ru>
2023-04-24 16:15:51 +0300

    recreate IBaseRepository

---
+++ BLL/Interfaces/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace BLL.Interfaces
+{
+    public interface IBaseService<T> where T : class
+    {
+        public Task<T> GetById(int id);
+        public Task<List<T>> GetAll();
+        public Task<T> Create(T item);
+        public Task<T> Remove(T item);
+        public Task<T> Update(T item);
+    }
+}
