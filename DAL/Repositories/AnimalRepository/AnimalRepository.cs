f8daa73a691b99722f740bf50392888284929ad6
Osten Power <osten_power@mail.ru>
2023-04-24 16:30:49 +0300

    create animalrepository

--- DAL/Repositories/AnimalRepository/AnimalRepository.cs
+++ DAL/Repositories/AnimalRepository/AnimalRepository.cs
@@ -4,10 +4,39 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Repositories.AnimalRepository
 {
-    internal class AnimalRepository : Repository<Animal>, IAnimalRepository
+    internal class AnimalRepository : BaseRepository<Animal>, IAnimalRepository
     {
         public AnimalRepository(ApplicationContext context) : base(context)
         {
         }
+
+        public async override Task<IEnumerable<Animal>> GetAllAsync()
+            => await _dbContext.Animals
+            .AsNoTracking()
+            .ToListAsync();
+
+        public async Task<IEnumerable<Animal>> GetAllWithIncludeAsync()
+             => await _dbContext.Animals
+            .Include(a => a.InformationAnimal)
+            .AsNoTracking()
+            .ToListAsync();
+
+        public async override Task<Animal?> GetByIdAsync(int id)
+            => await _dbContext.Animals
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+
+        public async Task<Animal?> GetByIdWithIncludeAsync(int id)
+          => await _dbContext.Animals
+            .Include(a => a.InformationAnimal)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+
+        public async override Task<IEnumerable<Animal>> GetByPredicateAsync(Func<Animal, bool> predicate)
+            => await _dbContext.Animals
+            .Include(a => a.InformationAnimal)
+            .AsNoTracking()
+            .Where(o => predicate(o))
+            .ToListAsync();
     }
 }
