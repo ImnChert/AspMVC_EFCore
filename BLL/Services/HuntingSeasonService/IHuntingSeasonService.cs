642b327e325b43654568ddd8590e9473dc5eea5e
Osten Power <osten_power@mail.ru>
2023-04-25 11:59:05 +0300

    update huntinseason

--- BLL/Services/HuntingSeasonService/IHuntingSeasonService.cs
+++ BLL/Services/HuntingSeasonService/IHuntingSeasonService.cs
@@ -3,9 +3,7 @@ using BLL.Interfaces;
 
 namespace BLL.Services.HuntingSeasonService
 {
-    public interface IHuntingSeasonService : IRepositoryService<HuntingSeasonDTO>
+    public interface IHuntingSeasonService : IBaseService<HuntingSeasonDTO, HuntingSeasonDTO>
     {
-        public IEnumerable<HuntingSeasonDTO> GetIncludeInfo();
-        public HuntingSeasonDTO GetByIdIncludeInfo(int id);
     }
 }
