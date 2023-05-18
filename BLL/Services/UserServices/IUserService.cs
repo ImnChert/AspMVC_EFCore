27170def3e9e930a12fda6b335c5d5a7c27cf482
Osten Power <osten_power@mail.ru>
2023-05-18 11:11:35 +0300

    add userservice

--- BLL/Services/UserServices/IUserService.cs
+++ BLL/Services/UserServices/IUserService.cs
@@ -1,6 +1,13 @@
-﻿namespace BLL.Services.UserServices
+﻿using BLL.DTOs;
+
+namespace BLL.Services.UserServices
 {
-    internal interface IUserService
+    public interface IUserService
     {
+        public Task<IEnumerable<UserDTO>> GetAllAsync();
+        public Task<UserDTO?> GetUserByIdAsync(int id);
+        public Task<UserDTO> DeleteUserAsync(UserDTO user);
+        public Task<UserDTO> UpdateUserAsync(UserDTO user);
+        public Task<IEnumerable<string>> UserRolesAsync(UserDTO user);
     }
 }
